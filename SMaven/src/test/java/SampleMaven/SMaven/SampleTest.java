810e77d2ab4d4e4093cb9eff6f4cb8d87ccf56b4
Aniruddha Dey <Aniruddha Dey@DESKTOP-0FJT81L>
2019-05-05 13:16:53 +0530

    added a dependency WebDriver Manager which will replace the system.setProperty

--- SMaven/src/test/java/SampleMaven/SMaven/SampleTest.java
+++ SMaven/src/test/java/SampleMaven/SMaven/SampleTest.java
@@ -4,6 +4,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class SampleTest {
 	
 	static WebDriver driver = null;
@@ -11,7 +13,10 @@ public class SampleTest {
 	@Test
 	public static void Test() throws InterruptedException {
 		 
-		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Aniruddha Dey\\Downloads\\Selenium driver\\chrome\\chromedriver.exe");
+		//System.setProperty("webdriver.chrome.driver", "C:\\Users\\Aniruddha Dey\\Downloads\\Selenium driver\\chrome\\chromedriver.exe");
+		
+		WebDriverManager.chromedriver().setup();
+		
 		driver = new ChromeDriver();
 		
 		driver.get("https://testng.org/doc/maven.html");
